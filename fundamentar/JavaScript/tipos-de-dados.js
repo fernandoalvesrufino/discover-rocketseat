04817d10f807067a0bba15a1445af9ea65e5c4eb
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-02 11:04:54 -0300

    Update tipos-de-dados.js

--- fundamentar/JavaScript/tipos-de-dados.js
+++ fundamentar/JavaScript/tipos-de-dados.js
@@ -49,3 +49,59 @@ A diferença dos dois é que null é considerado como um objeto vazio, ou seja, 
 */
 
 console.log(undefined === null)
+
+/*
+Object
+
+O tipo de dado Object é estrutural, contendo atributos (ou propriedades), e métodos (ou funcionalidades). Um objeto é denotado com os caracteres { }, atributos e métodos com nome: valor . Por exemplo: {nome: "João", idade: 20}
+*/
+
+console.log({})
+
+let fernando = { 
+  nome: "Fernando", 
+  idade: 26,
+  andar: function() {
+    console.log('Andando...')
+  },
+}
+
+console.log(fernando)
+console.log(fernando.andar())
+
+
+/*
+Array (lista de dados)
+
+O tipo de dados Array, ou vetor, também é estrutural, e consiste em uma lista de dados agrupados, um Array é denotado com os caracteres [ ] , por exemplo: [ "Leite", "Ovos", "Manteiga", 1, 2 ] .
+
+*/
+console.log([])
+
+let programador = [
+  'Fernando', 
+  'Dev', 
+  26
+]
+
+console.log(programador)
+
+let listaMercado = [
+  "leite",
+  "pão",
+  "laranja"
+]
+
+console.log(listaMercado)
+
+
+/*
+Conclusão
+
+Conforme o ECMAScript Standard (o padrão do orgão que padroniza o JavaScript) temos 9 tipos de dados organizados em 3 categorias: Primitive / Primitive Value, Structural e Structural Primitive. 
+
+- Os primitivos são: String, Number, Boolean, Undefined, Symbol, BigInt. 
+- Os estruturais são: Object (nele podemos colocar Array, Map, Set, Date...) e Function. 
+- O primitivo estrutural que consiste apenas no dado null
+
+*/
