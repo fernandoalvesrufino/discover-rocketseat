fc4911aab780a22eb532ea36fe22cfae51a0951a
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-02 10:50:44 -0300

    Update tipos-de-dados.js

--- fundamentar/JavaScript/tipos-de-dados.js
+++ fundamentar/JavaScript/tipos-de-dados.js
@@ -20,3 +20,32 @@ console.log(
 `A soma de
 duas unidades 
 é ${1+1}`)
+
+/*
+Numbers
+
+No JavaScript temos 4 tipos de números, os inteiros (por exemplo: 13), os reais ou float (por exemplo: 83.1), o Not a Number (NaN) e o infinito (Infinity).
+*/ 
+
+console.log(12+2)
+console.log(12.5/9)
+console.log(12.5/'asdfg')
+console.log(12.5 === Infinity) //O I deve ser escrito em maiúsculo
+
+/*
+Boolean
+
+No JavaScript, um dado boolean pode ter apenas 2 valores, verdadeiro (true) e falso (false).
+Toda lógica que usarmos na programação vamos encontrar verdadeiro ou falso (ex: if e else)
+*/
+let two = 2
+console.log(2 == two)
+
+/* 
+Undefined x Null
+
+No JavaScript temos o tipo de dados Undefined, que representa valores indefinidos, e o tipo de dados null, que são muitas vezes confundidos. 
+A diferença dos dois é que null é considerado como um objeto vazio, ou seja, algo indefinido não existe, já algo null existe mas não tem valor algum.
+*/
+
+console.log(undefined === null)
