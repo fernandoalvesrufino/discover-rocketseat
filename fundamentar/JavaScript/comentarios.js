96bb5e6540c673a2f94e619420d6a69396ac4105
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-02 10:32:27 -0300

    Create comentarios.js

---
+++ fundamentar/JavaScript/comentarios.js
@@ -0,0 +1,10 @@
+// Aqui eu tenho um comentario em linha
+
+/* 
+  Aqui eu tenho um comentario multi linha
+
+  O codigo abaixo escreve alguma mensagem no DevTools
+*/
+
+console.log('Bem vindos ao Starter')
+console.log('Não irá ignorar')
