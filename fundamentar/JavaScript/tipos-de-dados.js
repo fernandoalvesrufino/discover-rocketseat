c2877a6fbbb22f8ead9fb7242c30202f4f8705d5
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-02 10:43:03 -0300

    Create tipos-de-dados.js

---
+++ fundamentar/JavaScript/tipos-de-dados.js
@@ -0,0 +1,22 @@
+/*
+Para entendermos mais sobre os tipos de dados, podemos compará-los com elementos da gramática, para
+aprendermos precisamos saber como escrever, os significados e continuar aprendendo para expandir nosso 
+vocabulário.
+*/
+
+/*
+String
+- Cadeia de caracteres, ou seja, textos. 
+
+Para denotar string no JavaScript são usados aspas duplas ( " " ), aspas simples ( ' ' ) e template literals ou template strings (` `).
+
+Template literals permitem textos multilinhas e expressões de linguagem com os caracteres ${ }, por exemplo: console.log(`A soma de duas unidades é ${1+1}`), já as outras não, por exemplo: console.log("Isso é um texto").
+*/
+
+// Apossibilidade de colocar valores (ex: ${1+1}) é chamado de interpolação
+console.log(`A soma de duas unidades é ${1+1}`)
+
+console.log(
+`A soma de
+duas unidades 
+é ${1+1}`)
