647f13b5c4f981b7560e06fb39d7de769e9aa806
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-05 09:28:47 -0300

    Update array.js

--- fundamentar/JavaScript/array.js
+++ fundamentar/JavaScript/array.js
@@ -15,8 +15,24 @@ por exemplo: animals.length.
 const animals = [ 
   "Lion", 
   "Monkey",
-  "Cat",
+  {
+    name: "Cat",
+    age: 2
+  },
   "Dog"
 ]
+
+// para acessar algum elemento do array usamos [n]
+// Se não houver nenhum elemento na posição indicada, sera retornado undefined
+console.log(animals[7])
+
+// a posição dos elementos do array começa a ser contado a partir do 0 (zero)
 console.log(animals[0])
 console.log(animals.length)
+
+// posso também colocar objetos dentro de array e posso acessá-los com o .
+// Isso é possível, mas não é recomendado aplicar um objeto apenas para um elemento do array pois pode causar confusão
+console.log(animals[2].name)
+console.log(animals[2].age)
+
+console.log(animals[2])
