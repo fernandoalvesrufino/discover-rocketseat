5ab2f053cc6886f839e45d8a0a2a77b8cda5c91d
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-05 09:19:23 -0300

    Create array.js

---
+++ fundamentar/JavaScript/array.js
@@ -0,0 +1,22 @@
+/*
+Array
+
+Para criarmos um vetor utilizamos [ ] envolvendo os valores, por exemplo: 
+const animals = [ "Lion", "Monkey" ]
+
+para acessar esses valores usamos a posição do valor no vetor, começando por 0, por exemplo: 
+console.log(animals[0])
+
+Para descobrirmos o número de posições de um vetor qualquer, podemos utilizar a propriedade length, 
+por exemplo: animals.length.
+
+*/
+
+const animals = [ 
+  "Lion", 
+  "Monkey",
+  "Cat",
+  "Dog"
+]
+console.log(animals[0])
+console.log(animals.length)
