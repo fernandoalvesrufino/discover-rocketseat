c0db6f31fa285318dbcfae25b3595702a6953729
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-06 15:03:00 -0300

    Update funcoes.js

--- fundamentar/JavaScript/funcoes.js
+++ fundamentar/JavaScript/funcoes.js
@@ -25,17 +25,17 @@ Uma função é importante para o agrupamento e reutilização de código.
 
 // declaration - declaração da função
 // function statement
-function createSentences() {
-  console.log("Estudar é muito bom")
-  console.log("Paciência e persistência")
-  console.log("Revisão é a mãe do apredizado")
-}
+// function createSentences() {
+//   console.log("Estudar é muito bom")
+//   console.log("Paciência e persistência")
+//   console.log("Revisão é a mãe do apredizado")
+// }
 
 // Executar, rodar, chamar a função
-createSentences()
-createSentences()
+// createSentences()
+// createSentences()
 
-console.log("Fim do programa")
+// console.log("Fim do programa")
 
 /*
 Argumentos e parâmetros
@@ -52,8 +52,30 @@ Uma parte importante de funções são os parâmetros, que são similares variá
 
 // a função recebe os parameters (parâmetros)
 const sum = function(number1, number2) {
-  console.log(number1 + number2)
+  // console.log(number1 + number2)
+  let total = number1 + number2
+  return total
+  // No escopo da funcao, se eu criar uma variável do tipo var, ela não estará acessível como visto nos exemplos anteriores.
+  // Devemos tomar cuidado ao criar uma variável dentro de uma função. Pois se não definirmos o tipo da variável, ela pode ficar acessível nas demais partes do código. E isso é perigoso, pode trazer diversos problemas futuros.
 }
 
-sum(2, 3) // passamos arguments (argumentos) para a função
 
+let total = 0
+
+// sum(2, 3) // passamos arguments (argumentos) para a função
+let number1 = 34
+let number2 = 25
+
+// sum(number1, number2)
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${sum(number1, number2)}`) // Aqui só irá ser apresentado o valor da soma se o resultado for retornado. Senão, será impresso o resultado da função e só depois será apresentado a mensagem. PS: A mensagem retornara o resultado undefined, pois nenhum valor foi retornado
+console.log(total)
+// Esse total é diferente do total dentro da função
+
+/*
+return
+
+Para passar valores de dentro da função para o resto do código, usa-se a palavra reservada return seguida do valor à ser retornado, parando assim a execução da função e mandando o valor para onde a função foi chamada.
+*/
