e570c3149b21943a410b162ab23d4781fdc5a7f9
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-06 17:40:03 -0300

    Create manipulando-dados.js

---
+++ fundamentar/JavaScript/manipulando-dados.js
@@ -0,0 +1,19 @@
+/*
+- Prototype
+
+O JavaScript possui uma característica chamada Prototype, que é uma série de funcionalidades e atributos que já existem nativamente em diversos tipos de dados, que são lidados como objetos.
+*/
+
+// "Fernando".__proto__
+// console.log(["a", "b"].length)
+
+// Cada dado já está embrulhado em um objeto que pode ser manipulado/acessado
+
+/*
+- Type conversion (typecasting) vs Type coersion
+
+Type conversion se trata de uma conversão de tipo feita pelo dev, explicitamente, e pode ser feita de diversas maneiras, por exemplo com o método Number(), já type coersion é uma mudança de tipos feita pelo próprio JavaScript, implicitamente, por exemplo quando você tenta somar um número na forma string com um número do tipo número.
+*/
+
+console.log('9' + 5)
+console.log('9' + '5')
