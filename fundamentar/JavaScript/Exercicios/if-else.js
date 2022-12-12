9b735e7aa9d9f5955377773dd06dd1d35290ab68
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-12 14:55:17 -0300

    otimizando codigo, colocando em uma funcao

--- fundamentar/JavaScript/Exercicios/if-else.js
+++ fundamentar/JavaScript/Exercicios/if-else.js
@@ -11,28 +11,37 @@
 // * entre 60 - 69   -   D
 // * menor que 60    -   F
 
-let score = 70
+function getScore (score) {
+  let scoreA = score >= 90 && score <= 100
+  let scoreB = score >= 80 && score < 90
+  let scoreC = score >= 70 && score < 80
+  let scoreD = score >= 60 && score < 70
+  let scoreF = score >= 0 && score < 60
 
-let scoreA = score >= 90 && score <= 100
-let scoreB = score >= 80 && score < 90
-let scoreC = score >= 70 && score < 80
-let scoreD = score >= 60 && score < 70
-let scoreF = score >= 0 && score < 60
+  let ScoreFinal
 
-let ScoreFinal
+  if (scoreA) {
+    ScoreFinal = "A"
+  } else if (scoreB) {
+    ScoreFinal = "B"
+  } else if (scoreC) {
+    ScoreFinal = "C"
+  } else if (scoreD) {
+    ScoreFinal = "D"
+  } else if (scoreF) {
+    ScoreFinal = "F"
+  } else {
+    ScoreFinal = "Nota inválida"
+  }
 
-if (scoreA) {
-  ScoreFinal = "A"
-} else if (scoreB) {
-  ScoreFinal = "B"
-} else if (scoreC) {
-  ScoreFinal = "C"
-} else if (scoreD) {
-  ScoreFinal = "D"
-} else if (scoreF) {
-  ScoreFinal = "F"
-} else {
-  ScoreFinal = "Nota inválida"
+  return ScoreFinal
 }
 
-console.log(ScoreFinal)
+console.log(getScore(100))
+console.log(getScore(95))
+console.log(getScore(85))
+console.log(getScore(107))
+console.log(getScore(65))
+console.log(getScore(71))
+console.log(getScore(9))
+console.log(getScore(-9))
