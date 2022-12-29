33dfc6ddba2bc4953381fb317d74f32984a277c4
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-29 17:50:30 -0300

    globals

---
+++ fundamentar/Node.JS/globals.js
@@ -0,0 +1,7 @@
+console.log(global)
+
+// Para saber mais: nodejs.org > docs > versão instalada > globals
+
+
+console.log(__dirname) // diretório onde estou
+console.log(__filename) // nome do arquivo
