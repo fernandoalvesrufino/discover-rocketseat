9b39fe1b71d08b5c4effd8469582c80bbbedcbb2
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-29 18:00:56 -0300

    exports e require - criando um modulo para ser usado no node

---
+++ fundamentar/Node.JS/exports.js
@@ -0,0 +1,3 @@
+// Para exportar ou criar um modulo para ser usado dentro do node utilizamos o exports 
+
+module.exports = "Enviando dados do meu módulo"
--- fundamentar/Node.JS/require.js
+++ fundamentar/Node.JS/require.js
@@ -1,5 +1,12 @@
+// Modulos nativos 
 const path = require('path')
-
 console.log(path.basename(__filename))
 
 // Para usar modulos, usamos a função require. Assim conseguimos usar modulos nos nossos projetos 
+
+// ___________________________________
+
+// Meus modulos 
+// Não coloco o nome do modulo dentro da função e sim a localização
+const myModule = require('./exports')
+console.log(myModule)
