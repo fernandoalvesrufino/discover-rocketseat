4af8cf07d0db6f325249e96864d6edc22afab70f
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-29 17:47:28 -0300

    Iniciando com node js

---
+++ fundamentar/Node.JS/console.js
@@ -0,0 +1,12 @@
+console.log('Alguma coisa')
+global.console.log('Alguma coisa')
+
+// global faz parte das variaveis globais do node
+
+console.log(global)
+
+// Para saber mais: nodejs.org > docs > versão instalada > globals
+
+
+console.log(__dirname) // diretório onde estou
+console.log(__filename) // nome do arquivo
