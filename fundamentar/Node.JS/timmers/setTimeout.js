79e5c3526ed8d0182f8724a76d3cedf71c532dde
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-12-30 23:50:53 -0300

    Adicionado comentario sobre sincronismo e assincronismo

--- fundamentar/Node.JS/timmers/setTimeout.js
+++ fundamentar/Node.JS/timmers/setTimeout.js
@@ -4,3 +4,6 @@ const finished = () => console.log('done!')
 setTimeout(finished, timeOut)
 // finished é uma função callback. Ela vai ser chamada de volta após o tempo determinado em timeOut (3s)
 console.log('Mostrar')
+
+// Aqui tambem podemos ver o assincronismo. Pois o js vai executar o console.log('Mostrar') e depois vai imprimir o que foi definido no setTimeout 
+// Se fosse sincrono, seria executado linha por linha 
